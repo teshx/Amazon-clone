31ffbcfe8d3f4a75e6ec493f2e2945973b2811ff
teshager habtie <teshie00001@gmail.com>
2024-09-10 08:28:28 -0500

    Work on payment functionality

--- src/pages/Payments/Payements.jsx
+++ src/pages/Payments/Payements.jsx
@@ -5,6 +5,9 @@ import { useStripe, useElements, CardElement } from "@stripe/react-stripe-js";
 import { DataContext } from "../../components/Dataprovider/Dataprovider";
 import ProductCard from "../../components/products/ProductCard";
 import CurrencyFormat from "../../currecyFormat/Curencyformat";
+
+import { axiosInstance } from "../../Api/axios";
+import { ClipLoader } from "react-spinners";
 function Payements() {
   const [{ user, basket }] = useContext(DataContext);
   console.log(basket);
@@ -14,13 +17,42 @@ function Payements() {
   const stripe = useStripe();
   const elements = useElements();
   const [Carderror, setCarderror] = useState(null);
-
+  const [prossesing, setprocess] = useState(false);
   const handelchage = (e) => {
     e?.error?.message ? setCarderror(e?.error?.message) : setCarderror("");
-  }; 
+  };
   const total = basket.reduce((amount, item) => {
     return item.price * item.amount + amount;
   }, 0);
+
+  const handelePayment = async (e) => {
+    e.preventDefault();
+    setprocess(true);
+    // 1; // contact backend function---> cliant secrit
+    try {
+      const response = await axiosInstance({
+        method: "POST",
+        url: `/payment/create?total=${total * 100}`,
+      });
+
+      const clintSecret = response.data?.client_secret;
+
+      // 2; //Cliant side conformation
+      const conformation = await stripe.confirmCardPayment(clintSecret, {
+        payment_method: {
+          card: elements.getElement(CardElement),
+        },
+      });
+
+      console.log(conformation);
+      setprocess(false);
+    } catch (error) {
+      console.log(error);
+      setprocess(false);
+    }
+
+    // 3; // after confirm -->store db orders and clean basket
+  };
 
   return (
     <Layout>
@@ -71,7 +103,16 @@ function Payements() {
                       <p> Total order |</p> <CurrencyFormat amount={total} />
                     </span>
                   </div>
-                  <button>Pay Now</button>
+                  <button type="submit">
+                    {prossesing ? (
+                      <div className={classes.loders}>
+                        <ClipLoader color="grey" size={12} />
+                        <p> please wait..</p>
+                      </div>
+                    ) : (
+                      "Pay Now"
+                    )}
+                  </button>
                 </div>
               </form>
             </div>
