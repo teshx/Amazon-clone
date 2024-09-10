9bb2c1c0757e552889c1e05f1b28ee3c2466597a
teshager habtie <teshie00001@gmail.com>
2024-09-10 07:05:39 -0500

    work on payment card

--- src/Router.jsx
+++ src/Router.jsx
@@ -7,6 +7,13 @@ import Orders from "./pages/Orders/Orders";
 import Cart from "./pages/Cart/Cart";
 import Landing from "./pages/Landing/Landing";
 import Results from "./pages/Results/Results";
+
+import { Elements } from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
+const stripePromise = loadStripe(
+  "pk_test_51PwpMuK2PpLTPswq4gn1KIJExnrS87scICELxWxxyJ08e50Hg6RXKTlObfvCkh9YUJwCzvD261v2AgdEMRdax7Z400GXX3DPqa"
+);
+
 function Routers() {
   return (
     <>
@@ -14,7 +21,14 @@ function Routers() {
         <Routes>
           <Route path="/" element={<Landing />} />
           <Route path="/auth" element={<Auth />} />
-          <Route path="/payement" element={<Payemnt />} />
+          <Route
+            path="/payement"
+            element={
+              <Elements stripe={stripePromise}>
+                <Payemnt />
+              </Elements>
+            }
+          />
           <Route path="/order" element={<Orders />} />
           <Route path="/category/:categoryName" element={<Results />} />
           <Route path="/products/:id" element={<Products />} />
--- src/pages/Payments/Payements.jsx
+++ src/pages/Payments/Payements.jsx
@@ -1,15 +1,27 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import classes from "./payment.module.css";
 import Layout from "../../components/Layout/Layout";
-
+import { useStripe, useElements, CardElement } from "@stripe/react-stripe-js";
 import { DataContext } from "../../components/Dataprovider/Dataprovider";
 import ProductCard from "../../components/products/ProductCard";
+import CurrencyFormat from "../../currecyFormat/Curencyformat";
 function Payements() {
   const [{ user, basket }] = useContext(DataContext);
   console.log(basket);
   const totalItem = basket?.reduce((amount, item) => {
     return item.amount + amount;
   }, 0);
+  const stripe = useStripe();
+  const elements = useElements();
+  const [Carderror, setCarderror] = useState(null);
+
+  const handelchage = (e) => {
+    e?.error?.message ? setCarderror(e?.error?.message) : setCarderror("");
+  }; 
+  const total = basket.reduce((amount, item) => {
+    return item.price * item.amount + amount;
+  }, 0);
+
   return (
     <Layout>
       {/* header */}
@@ -41,6 +53,29 @@ function Payements() {
         <hr />
         <div className={classes.flex}>
           <h3>Paymemnt mathod</h3>
+          <div className={classes.payement_Card_container}>
+            <div className={classes.Payement_details}>
+              <form action="" onSubmit={handelePayment}>
+                {/* error */}
+                {Carderror && (
+                  <small style={{ color: "red" }}>{Carderror}</small>
+                )}
+                {/* card element */}
+                <CardElement onChange={handelchage} />
+
+                {/* price */}
+
+                <div className={classes.payement_prices}>
+                  <div>
+                    <span style={{ display: "flex", gap: "15px" }}>
+                      <p> Total order |</p> <CurrencyFormat amount={total} />
+                    </span>
+                  </div>
+                  <button>Pay Now</button>
+                </div>
+              </form>
+            </div>
+          </div>
         </div>
         {/* address */}
       </section>
